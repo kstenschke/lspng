b6b78ed451913db6ff296c034ad6416000968a67
kay.stenschke <stenschke@gyselroth.com>
2021-05-22 00:34:30 +0200

    update copyright comment

--- lspng/main.cc
+++ lspng/main.cc
@@ -7,7 +7,7 @@
  * This is free software: you are free to change and redistribute it.
  * There is NO WARRANTY, to the extent permitted by law.
  *
- * Written by Kay Stenschke, see <https://github.com/kstenschke/grepc>.
+ * Written by Kay Stenschke, see <https://github.com/kstenschke/lspng>.
  */
 
 #define cimg_use_png 1
